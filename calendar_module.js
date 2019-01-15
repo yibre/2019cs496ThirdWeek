cb3bd17fd1bb50756e64dbf010d9929041881194
Hyeonjoon <46161491+Hyeonjoon@users.noreply.github.com>
2019-01-15 14:35:00 +0900

    Modified modules.

--- calendar_module.js
+++ calendar_module.js
@@ -5,24 +5,38 @@
 import {getSchedule, postSchedule} from './http_module.js';
  /*
   * getContent - Call back function for 'getSchedule' in http_module.js.
-  *              
+  d*              
   */
- export function getContent(data){
-     // Implemented assuming that the 'data' is JSON object type.
-     var info  = JSON.parse(data).info;
-     // info.date와 info.content를 html로 매칭해줘야 할 듯!
- }
+export function getContent(data){
+    // Implemented assuming that the 'data' is JSON object type.
+    var info  = data.info;
+    for (i = 1; i < 32; i++){
+        var id = i.toString();
+        var content = info[i - 1].content;
+        document.getElementById(id).value = content;
+    }
+}
 
- /*
-  * postContent - Construct data which will be posted to node.js server.
-  *               Call 'postSchedule' in http_module.js as a call back function.
-  *               Called if 'save' button is clicked.
-  */
- export function postContent(username, year_month){
-     // 저장 시의 date : content 쌍을 모두 긁어와 JSON data로 만들어주는 작업.
-     // 해당 JSON data를 postSchedule에게 넘겨줘야 함.
+/*
+ * postContent - Construct data which will be posted to node.js server.
+ *               Call 'postSchedule' in http_module.js as a call back function.
+ *               Called if 'save' button is clicked.
+ */
+export function postContent(username, year_month){
+    // 저장 시의 date : content 쌍을 모두 긁어와 JSON data로 만들어주는 작업.
+    // 해당 JSON data를 postSchedule에게 넘겨줘야 함.
 
-     var data = '';// JSON data.
+    var json = {};
+    json.info = [];
+    
+    for (i = 1; i < 32; i++){
+        var id = i.toString();
+        var docu = {};
+        docu['date'] = id;
+        
+        docu['content'] = document.getElementById(id).value;
+        json.info.push(docu);
+    }
 
-     postSchedule(username, year_month, data);
- }
+    postSchedule(username, year_month, json);
+}
