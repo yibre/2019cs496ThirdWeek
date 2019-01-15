ecc43bdb3f5fa382dcfabbbaaa4d8edf16eb73e2
Hyeonjoon <46161491+Hyeonjoon@users.noreply.github.com>
2019-01-15 14:39:28 +0900

    Modified comments.

--- calendar_module.js
+++ calendar_module.js
@@ -2,13 +2,14 @@
  * calendar_module.js - Module which includes functions
  *                      deal with connection between Front-end and Back-end.
  */
+
 import {getSchedule, postSchedule} from './http_module.js';
+
  /*
   * getContent - Call back function for 'getSchedule' in http_module.js.
-  d*              
+  *              Parse JSON data get from server and pass it to html.
   */
 export function getContent(data){
-    // Implemented assuming that the 'data' is JSON object type.
     var info  = data.info;
     for (i = 1; i < 32; i++){
         var id = i.toString();
@@ -23,9 +24,6 @@ export function getContent(data){
  *               Called if 'save' button is clicked.
  */
 export function postContent(username, year_month){
-    // 저장 시의 date : content 쌍을 모두 긁어와 JSON data로 만들어주는 작업.
-    // 해당 JSON data를 postSchedule에게 넘겨줘야 함.
-
     var json = {};
     json.info = [];
     
@@ -37,6 +35,6 @@ export function postContent(username, year_month){
         docu['content'] = document.getElementById(id).value;
         json.info.push(docu);
     }
-
+    // Call postSchedule as a call back function.
     postSchedule(username, year_month, json);
 }
